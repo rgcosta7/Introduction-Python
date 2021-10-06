2daa5e1874702231675ec4b9fbd05cb2519e371e
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-10-06 13:19:46 +0100

    Objectives: experimenting with existing Python code; discovering and fixing basic syntax errors; becoming familiar with the print() function and its formatting capabilities.

---
+++ Lab 2.1.1.19.py
@@ -0,0 +1,20 @@
+'''
+Name: Raul Costa
+Date: 13/09/2021
+'''
+
+print("       *        \n      * *       \n     *   *      \n    *     *     \n   *       *    \n  *         *   \n *           *  ")
+print("***         *** \n  *         *   \n  *         *   \n  ***********   ")
+
+
+print("       *        " *2)
+print("      * *       " *2)
+print("     *   *      " *2)
+print("    *     *     " *2)
+print("   *       *    " *2)
+print("  *         *   " *2)
+print(" *           *  " *2)
+print("***         *** " *2)
+print("  *         *   " *2)
+print("  *         *   " *2)
+print("  ***********   " *2)
