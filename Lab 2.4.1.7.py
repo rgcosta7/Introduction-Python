dbff634661b09792f43e94840c5e8d214c47efd2
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-10-06 13:25:24 +0100

    Lab 2.4.1.7
    Becoming familiar with the concept of storing and working with different data types in Python;

---
+++ Lab 2.4.1.7.py
@@ -0,0 +1,9 @@
+'''
+Name: Raul Costa
+Date: 30/09/2021
+john = 3
+mary = 5
+adam = 6
+print(john, mary, adam,  sep=',')
+total_apples = john + mary + adam
+print("Total number of apples:", total_apples  )
