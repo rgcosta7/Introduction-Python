e5f48107ccb253178306849dae91afe4c457755a
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-10-11 10:37:05 +0100

    Create Lab 2.5.1.2.py
    becoming familiar with the concept of comments in Python;
    using and not using comments;
    replacing comments with code;
    experimenting with Python code.

---
+++ Lab 2.5.1.2.py
@@ -0,0 +1,13 @@
+# Basic hours to seconds program
+# Name: Raul Costa
+# Date: 11/10/2021
+# Version: 1.0
+
+hours = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", hours) # printing the number of hours
+print("Seconds in Hours: ", hours * seconds) # printing the number of seconds in a given number of hours
+
+# This is the end of the program that computes the number of seconds in 3 hour
+pring("Goodbye")
