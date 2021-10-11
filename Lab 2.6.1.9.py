94d3043b45c74e41092b207b66ee34f2f3272de9
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-10-11 10:23:17 +0100

    Create Lab 2.6.1.9.py
    becoming familiar with the inputting and outputting of data in Python;
    evaluating simple expressions.

---
+++ Lab 2.6.1.9.py
@@ -0,0 +1,21 @@
+'''
+Basic arithmetic operations program
+Name: Raul Costa
+Date: 04/10/2021
+'''
+
+# input a float value for variable a here
+first_n = int(input("Enter your first number\n"))
+# input a float value for variable b here
+second_n = int(input("Enter your second number\n"))
+
+# output the result of addition
+print(f'The result of addition between {first_n} and {second_n} is {first_n + second_n}')
+# output the result of subtraction
+print(f'The result of addition between {first_n} and {second_n} is {first_n - second_n}')
+# output the result of multiplication
+print(f'The result of addition between {first_n} and {second_n} is {first_n * second_n}')
+# output the result of division 
+print(f'The result of addition between {first_n} and {second_n} is {first_n / second_n}')
+
+print("\nThat's all, folks!")
