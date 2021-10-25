fb65405f664aecd11836c756e763b48ef85b6b4f
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-10-25 11:00:18 +0100

    Update Lab 2.6.1.10.py
    Add Comments to the code

--- Lab 2.6.1.10.py
+++ Lab 2.6.1.10.py
@@ -1,12 +1,14 @@
 '''
 Arithmetic operations program
 Name: Raul Costa
-Date: 18/10/2021
-Version: 1.0
+Date: 11/10/2021
+Version: 1.1
 '''
-
+# Ask input to the user
 x = float(input("Enter value for x: "))
 
+# Calculate the result 
 y =1/(x+1/(x+1/(x+1/x)))
 
+# Print the result
 print("y =", y)
