4b305c01b7d8ed4d3fbdc95935365a19175ce93c
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-11-08 10:22:04 +0000

    Update Lab 2.5.1.2.py
    Typo error on print

--- Lab 2.5.1.2.py
+++ Lab 2.5.1.2.py
@@ -10,4 +10,4 @@ print("Hours: ", hours) # printing the number of hours
 print("Seconds in Hours: ", hours * seconds) # printing the number of seconds in a given number of hours
 
 # This is the end of the program that computes the number of seconds in 3 hour
-pring("Goodbye")
+print("Goodbye")
