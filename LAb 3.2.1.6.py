4862bc812aa596c27df697358340c7ef094b0bf6
Raul Costa <91840903+rgcosta7@users.noreply.github.com>
2021-11-15 09:50:48 +0000

    Lab 3.2.1.6
    Objectives
    Familiarize the student with:
    
    using the for loop;
    reflecting real-life situations in computer code.

---
+++ LAb 3.2.1.6.py
@@ -0,0 +1,17 @@
+'''
+Count mississippily
+Name: Raul Costa
+Date: 15/11/2021
+Version: 1.0
+'''
+# Inport time library
+import time
+
+# Loop five times
+for i in range(1,6):
+    # Print the loop number and the word Mississippi
+    print(f'{i} Mississippi')
+    # Wait 1 second before repeat the loop
+    time.sleep(1)
+# Print when the loop finished    
+print("Ready or not, here I come!")
